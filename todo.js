b0c9b52ee0276e95cb606e756d4ab64f6eb14a4b
Param kamdar <62197871+param623@users.noreply.github.com>
2020-06-06 16:52:54 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,23 @@
+//this is for toggle css of li's.
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type = 'text'").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();	
+});
